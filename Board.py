56a89ccd5c1c1868f1b13bf60a44d71725fa6bb4
upnorthjay919 <upnorthjay919@yahoo.com>
2021-12-04 11:02:36 -0500

    Test 2

---
+++ Board.py
@@ -0,0 +1,19 @@
+#Tic Tac Toe Board
+myCount = 1
+
+while(myCount < 5):
+    print(myCount)
+
+    if (myCount == 5):
+        print("myCount")
+    myCount = myCount + 1
+
+
+#while loop
+myCount =2
+
+while(myCount < 20):
+    print(myCount)
+    if (myCount % 2 == 0):
+        print(myCount)
+    myCount = myCount + 1
